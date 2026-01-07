c43577b3a11a49acf4acf873d344d7ea62a7573c
Ígor José Rodrigues <igorjr2003@gmail.com>
2026-01-07 16:17:12 -0300

    feat: add backend test request in frontend

--- apps/frontend/src/app/app.ts
+++ apps/frontend/src/app/app.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, ChangeDetectorRef } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { delay } from 'rxjs/operators';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule, CommonModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected title = 'frontend';
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  private cdr = inject(ChangeDetectorRef);
+
+  protected apiMessage = '';
+  protected loading = true;
+  protected error = '';
+
+  ngOnInit() {
+    this.http
+      .get('/api')
+      .pipe(delay(1000))
+      .subscribe({
+        next: (data) => {
+          this.apiMessage = (data as { message: string }).message;
+          this.loading = false;
+          this.cdr.detectChanges();
+        },
+        error: (err) => {
+          this.error = 'Backend connection error: ' + err.message;
+          this.loading = false;
+          this.cdr.detectChanges();
+        },
+      });
+  }
 }
