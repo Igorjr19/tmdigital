0cda6f5e53f5cfc099614bcc9a09537e993ab120
Ígor José Rodrigues <igorjr2003@gmail.com>
2026-01-09 17:52:15 -0300

    refactor: remove layout component

--- apps/frontend/src/app/app.ts
+++ apps/frontend/src/app/app.ts
@@ -1,15 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, MenubarModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App implements OnInit {
+  items: MenuItem[] | undefined;
+
   ngOnInit() {
-    return;
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        routerLink: '/',
+      },
+      {
+        label: 'Leads',
+        icon: 'pi pi-users',
+        routerLink: '/leads',
+      },
+    ];
   }
 }
