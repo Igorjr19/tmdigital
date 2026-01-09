1d42bfdf5cab96d3383b4f8b80e6890ee1880b68
Ígor José Rodrigues <igorjr2003@gmail.com>
2026-01-09 17:36:20 -0300

    feat: generate api

---
+++ apps/frontend/src/app/api/api/api.ts
@@ -0,0 +1,5 @@
+export * from './leads.service';
+import { LeadsService } from './leads.service';
+export * from './rural-properties.service';
+import { RuralPropertiesService } from './rural-properties.service';
+export const APIS = [LeadsService, RuralPropertiesService];
