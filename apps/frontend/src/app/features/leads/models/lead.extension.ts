3555af51c119dfa5e372244642cb4b44e29e6fb4
Ígor José Rodrigues <igorjr2003@gmail.com>
2026-01-10 17:55:08 -0300

    fix: use point dto instead of declaring new type

--- apps/frontend/src/app/features/leads/models/lead.extension.ts
+++ apps/frontend/src/app/features/leads/models/lead.extension.ts
@@ -1,15 +1,10 @@
-import { LeadDto, RuralPropertyDto } from '../../../api/model/models';
-
-export interface GeoPoint {
-  type: 'Point';
-  coordinates: [number, number];
-}
+import { LeadDto, PointDto, RuralPropertyDto } from '../../../api/model/models';
 
 export interface RuralPropertyWithLocation extends Omit<
   RuralPropertyDto,
   'location'
 > {
-  location: GeoPoint;
+  location: PointDto;
 }
 
 export interface LeadWithProperties extends LeadDto {
