9a066a4920e77d00e45ab0918dd28942d6bdad33
Ígor José Rodrigues <igorjr2003@gmail.com>
2026-01-13 10:08:31 -0300

    fix: map popup format status

--- apps/frontend/src/app/features/leads/components/map-popup/map-popup.component.ts
+++ apps/frontend/src/app/features/leads/components/map-popup/map-popup.component.ts
@@ -6,13 +6,14 @@ import {
   Input,
   Output,
 } from '@angular/core';
+import { TagModule } from 'primeng/tag';
 import { LeadDto } from '../../../../api/model/models';
 import { I18N } from '../../../../core/i18n/i18n';
 
 @Component({
   selector: 'app-map-popup',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TagModule],
   templateUrl: './map-popup.component.html',
   styleUrl: './map-popup.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -28,5 +29,25 @@ export class MapPopupComponent {
   get translatedStatus(): string {
     const status = this.leadStatus as LeadDto.StatusEnum;
     return I18N.LEAD.STATUS[status] || (this.leadStatus as string);
+  }
+
+  getSeverity(
+    status: unknown,
+  ): 'success' | 'info' | 'warn' | 'danger' | undefined {
+    const s = status as string;
+    switch (s) {
+      case LeadDto.StatusEnum.New:
+      case LeadDto.StatusEnum.Converted:
+      case 'Approved':
+        return 'success';
+      case LeadDto.StatusEnum.Qualified:
+        return 'info';
+      case LeadDto.StatusEnum.Contacted:
+        return 'warn';
+      case LeadDto.StatusEnum.Lost:
+        return 'danger';
+      default:
+        return undefined;
+    }
   }
 }
